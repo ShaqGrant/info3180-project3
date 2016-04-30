5685c375e0cc5bd2b4ae9c8e23d976da66564bbf
shaqgrant <shaq.grant.95@gmail.com>
2016-04-30 00:34:50 +0000

    project 4

--- app/static/js/usersController.js
+++ app/static/js/usersController.js
@@ -1,4 +1,4 @@
-angular.module('WishList').controller('UsersController',['$scope','$location','$cookies','APIService',function($scope,$location,$cookies, APIService){
+angular.module('WishList').controller('UsersController',['$scope','$location','$cookies','$uibModal','APIService',function($scope,$location,$cookies,$uibModal,APIService){
     $scope.currentUserName = $cookies.get('userName');
     $scope.currentUserId = $cookies.get('userId');
     if($cookies.get('loggedIn')!='true'){
@@ -13,10 +13,30 @@ angular.module('WishList').controller('UsersController',['$scope','$location','$
     });
     
     $scope.seeWishlist = function(userid){
-        console.log(userid);
         APIService.getWishes(userid)
         .then(function(data){
-            console.log(data);
-        })
-    }
+            if(data.message=="Success"){
+                var modalInstance = $uibModal.open({
+                    templateUrl : 'static/templates/viewwishes.html',
+                    controller : 'ViewWishesModal',
+                    size: 'md',
+                    animation: true,
+                    resolve: {
+                        result : function(){
+                            return data;
+                        }
+                    }
+                });
+            }
+        });
+    };
 }]);
+
+angular.module('WishList').controller('ViewWishesModal',function($scope,$uibModalInstance,result){
+    $scope.wishes = result.data.wishes;
+    $scope.user = result.data.user;
+    
+    $scope.ok= function (){
+        $uibModalInstance.dismiss();
+    };
+});
